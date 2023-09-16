a7003b0ccd8f5181a9e7086c62861e034eefa931
辻田慎悟 <sin5kyowakoku@gmail.com>
2023-09-16 22:16:43 +0900

    Add : ロビーシーンの処理を行うスクリプト

---
+++ GunGame/Assets/Scripts/Scenes/LobbyProc.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyProc : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+}
+
+
+
+
+
